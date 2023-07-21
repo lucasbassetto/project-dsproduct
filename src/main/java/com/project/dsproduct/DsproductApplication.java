fe09083c7aa863376006545ea7b1879c4a1c491c
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-07-21 12:22:30 -0300

    Project created, properties

---
+++ src/main/java/com/project/dsproduct/DsproductApplication.java
@@ -0,0 +1,13 @@
+package com.project.dsproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsproductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dsproduct/DsproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dsproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
