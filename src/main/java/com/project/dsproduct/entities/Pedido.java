30a86eef487f444115109bb06107bab65ea96da1
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-07-21 12:37:35 -0300

    Game entity, ORM

---
+++ src/main/java/com/project/dsproduct/entities/Pedido.java
@@ -0,0 +1,53 @@
+package com.project.dsproduct.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Instant date;
+
+    public Pedido() {
+    }
+
+    public Pedido(Integer id, Instant date) {
+        this.id = id;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pedido pedido = (Pedido) o;
+        return id.equals(pedido.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
